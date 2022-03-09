beb28faf00ee4cfdc14f7d1ed027ccf6b4c80697
Samandar003 <samandar200527@gmail.com>
2022-03-09 23:30:58 +0500

    added auth

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
+import datetime
 from django.http import HttpResponse
+import jwt
 from rest_framework.views import APIView
 from rest_framework.exceptions import AuthenticationFailed
 from users.models import User
@@ -24,7 +26,38 @@ class LoginView(APIView):
       raise AuthenticationFailed('User note found')
     if not user.check_password(password):
       raise AuthenticationFailed('Incorrect password')
-    return Response({
-        "message":"success"
-      })
     
+    payload = {
+      "id":user.id,
+      "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+      "iat":datetime.datetime.utcnow()
+    }
+    token = jwt.encode(payload, 'secret', algorithm='HS256')
+    response = Response()
+    response.set_cookie(key='jwt', value=token, httponly=True)
+    response.data = {
+      'jwt':token
+    }
+    return response
+  
+class UserView(APIView):
+  def get(self, request):
+    token = request.COOKIES.get('jwt')
+    if not token:
+      raise AuthenticationFailed('Unauthenticated!')
+    try:
+      payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+    except jwt.ExpiredSignatureError:
+      raise AuthenticationFailed('Unauthenticated!')
+    user = User.objects.filter(id=payload['id']).first()
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+  
+class LogoutView(APIView):
+  def post(self, request):
+    response = Response()
+    response.delete_cookie('jwt')
+    response.data = {
+      "message":"success"
+    }
+    return response
